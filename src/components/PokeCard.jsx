f6477421945958ae35a3ddeb1cc57ffb33fa8f2e
jcartronics <jcar-95@hotmail.com>
2023-06-30 23:31:27 -0400

    Agrega lista de pokemones y detalle

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -30,20 +30,15 @@ export const PokeCard = () => {
         })
 }, [id])
 
-console.log(pokemon);
-console.log(pokemon['stats'][0]['base_stat'])
   return (
     <div>
       <Card className='pokeCard' style={{ width: '18rem' }}>
           <Card.Img variant="top" src={imgPoke} />
           <Card.Body>
               <Card.Title>{pokemon.name}</Card.Title>
-              <Card.Text>{pokemon['stats'][0]['stat']['name']}: {pokemon['stats'][0]['base_stat']}</Card.Text>
-              <Card.Text>{pokemon['stats'][1]['stat']['name']}: {pokemon['stats'][1]['base_stat']}</Card.Text>
-              <Card.Text>{pokemon['stats'][2]['stat']['name']}: {pokemon['stats'][2]['base_stat']}</Card.Text>
-              <Card.Text>{pokemon['stats'][3]['stat']['name']}: {pokemon['stats'][3]['base_stat']}</Card.Text>
-              <Card.Text>{pokemon['stats'][4]['stat']['name']}: {pokemon['stats'][4]['base_stat']}</Card.Text>
-              <Card.Text>{pokemon['stats'][5]['stat']['name']}: {pokemon['stats'][5]['base_stat']}</Card.Text>
+              <Card.Text>Weight: {pokemon.weight}</Card.Text>
+              <Card.Text>Base Experience: {pokemon.base_experience}</Card.Text>
+              <Card.Text>Height: {pokemon.height}</Card.Text>
               
               <Button variant="warning" onClick={volver}>Volver</Button>
           </Card.Body>
--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -1,17 +1,49 @@
 import Button from 'react-bootstrap/Button'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const PokeList = () => {
+
+  // Api
+  const url = 'https://pokeapi.co/api/v2/pokemon';
+
+  // useNavigate
+  const navigate = useNavigate();
+
+  //Estados
+  const [pokeList, setPokeList] = useState([]);
+  const [pokemonSeleccionado, setPokemonSeleccionado] = useState('');
+
+  const elegirPokemon = () => {
+    if (pokemonSeleccionado !== ''){
+      navigate(`/pokemon/${pokemonSeleccionado}`)
+    } 
+  }
+
+  useEffect(() =>  {
+    fetch(url).then(res => res.json())
+    .then(data => {
+      console.log(data)
+        setPokeList(data.results.map(result => result.name))
+        console.log(data.results)
+    })
+}, [])
+
   return (
     <div className='container'>
       <h1>Selecciona un pokemon</h1>
       <div className='pokemonSeleccionado'>
-        <select>
+        <select onChange={(e) => setPokemonSeleccionado(e.target.value)}>
           <option value="">Elige tu pokemon</option>
 
           {/* //Agrega opciones desde api */}
+          {
+            pokeList.map(pokemon => {
+              return <option key={pokemon} value={pokemon}>{pokemon}</option>
+            })
+          }
         </select>
-        <Button variant="dark">Ver Detalle</Button>
+        <Button variant="dark" onClick={elegirPokemon}>Ver Detalle</Button>
       </div>
     </div>
   )
