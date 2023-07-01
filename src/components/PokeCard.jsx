eda62e592ea4f7a0bb50bbecf2b133fd70d12d94
jcartronics <jcar-95@hotmail.com>
2023-06-30 22:46:32 -0400

    Agrega ficha pokemon

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 export const PokeCard = () => {
 
@@ -29,8 +31,23 @@ export const PokeCard = () => {
 }, [id])
 
 console.log(pokemon);
-
+console.log(pokemon['stats'][0]['base_stat'])
   return (
-    <div>PokeCard</div>
+    <div>
+      <Card className='pokeCard' style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={imgPoke} />
+          <Card.Body>
+              <Card.Title>{pokemon.name}</Card.Title>
+              <Card.Text>{pokemon['stats'][0]['stat']['name']}: {pokemon['stats'][0]['base_stat']}</Card.Text>
+              <Card.Text>{pokemon['stats'][1]['stat']['name']}: {pokemon['stats'][1]['base_stat']}</Card.Text>
+              <Card.Text>{pokemon['stats'][2]['stat']['name']}: {pokemon['stats'][2]['base_stat']}</Card.Text>
+              <Card.Text>{pokemon['stats'][3]['stat']['name']}: {pokemon['stats'][3]['base_stat']}</Card.Text>
+              <Card.Text>{pokemon['stats'][4]['stat']['name']}: {pokemon['stats'][4]['base_stat']}</Card.Text>
+              <Card.Text>{pokemon['stats'][5]['stat']['name']}: {pokemon['stats'][5]['base_stat']}</Card.Text>
+              
+              <Button variant="warning" onClick={volver}>Volver</Button>
+          </Card.Body>
+      </Card>
+</div>
   )
 }
