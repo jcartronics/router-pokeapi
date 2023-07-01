990d74d7690d30b5118e41295dfb6d54cb961c80
jcartronics <jcar-95@hotmail.com>
2023-06-30 21:31:54 -0400

    Agrega Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+
+  const setActive = ({ isActive }) => isActive ? 'active' : 'inactive';
+
   return (
-    <div>Navbar</div>
+    <div className='nav'>
+      <NavLink to='/' className={setActive}>Home</NavLink>
+      <NavLink to='/pokemon' className={setActive}>Pokemon</NavLink>
+    </div>
   )
 }
