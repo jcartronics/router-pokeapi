e047eb125d70bf692b4a4fb81750df0ff24c6522
jcartronics <jcar-95@hotmail.com>
2023-06-30 22:04:24 -0400

    Agrega estructura componente PokeList

--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -1,7 +1,18 @@
+import Button from 'react-bootstrap/Button'
 import React from 'react'
 
 export const PokeList = () => {
   return (
-    <div>PokeList</div>
+    <div className='container'>
+      <h1>Selecciona un pokemon</h1>
+      <div className='pokemonSeleccionado'>
+        <select>
+          <option value="">Elige tu pokemon</option>
+
+          {/* //Agrega opciones desde api */}
+        </select>
+        <Button variant="dark">Ver Detalle</Button>
+      </div>
+    </div>
   )
 }
