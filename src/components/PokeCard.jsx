126304807fb6ba2ae33f45eeb671af66ee575eee
jcartronics <jcar-95@hotmail.com>
2023-06-30 22:21:31 -0400

    Conexion con API

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export const PokeCard = () => {
+
+  //Api
+  const url = 'https://pokeapi.co/api/v2/pokemon'
+
+  const { id } = useParams();
+
+  // Estados
+  const [pokemon, setPokemon] = useState({});
+  const [imgPoke, setImgPoke] = useState();
+
+  // useNavigate
+  const navigate = useNavigate()
+
+  const volver = () => {
+    navigate('/pokemon')
+  }
+
+// Actualiza al cambiar id
+  useEffect(() => {
+    fetch(`${url}/${id}`).then(res => res.json())
+        .then(data => {
+            setPokemon(data)
+            setImgPoke(data['sprites']['other']['official-artwork']['front_default'])
+        })
+}, [id])
+
+console.log(pokemon);
+
   return (
     <div>PokeCard</div>
   )
