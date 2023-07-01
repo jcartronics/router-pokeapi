979b1dc4f178640a378523849f2d1eecd8792496
jcartronics <jcar-95@hotmail.com>
2023-06-30 21:09:53 -0400

    Agrega Router y Rutas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+
+// Componentes
+import { Navbar } from './components/Navbar';
+import { PokeCard } from './components/PokeCard';
+import { Home } from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PokeList } from './components/PokeList';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={Home} />
+          <Route path='/pokemon' element={PokeList} />
+          <Route path='/pokemon/:id' element={PokeCard} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
